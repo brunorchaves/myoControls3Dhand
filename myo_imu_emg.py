740b984c5a58ab334ba7369d39422a50f252111b
Bruno <brunorchaves1@gmail.com>
2024-01-02 02:29:42 -0300

    rotation being sent to unity

--- myo_imu_emg.py
+++ myo_imu_emg.py
@@ -39,15 +39,19 @@ def euler_from_quaternion(x, y, z, w):
     t0 = +2.0 * (w * x + y * z)
     t1 = +1.0 - 2.0 * (x * x + y * y)
     roll_x = math.atan2(t0, t1)
-
+    roll_x = int(math.degrees(roll_x))
+    
     t2 = +2.0 * (w * y - z * x)
     t2 = +1.0 if t2 > +1.0 else t2
     t2 = -1.0 if t2 < -1.0 else t2
     pitch_y = math.asin(t2)
+    pitch_y = int(math.degrees(pitch_y))
+
 
     t3 = +2.0 * (w * z + x * y)
     t4 = +1.0 - 2.0 * (y * y + z * z)
     yaw_z = math.atan2(t3, t4)
+    yaw_z = int(math.degrees(yaw_z))
 
     return roll_x, pitch_y, yaw_z  # in radians
 
@@ -71,12 +75,13 @@ if __name__ == '__main__':
             if None not in emg[:7] and None not in euler_angles:
                 emg_float = [float(element) for element in emg]
                 yaw, pitch, roll = euler_angles
-                data = f"{control_value},{yaw},{pitch},{roll}"
+                # data = f"{control_value},{yaw},{pitch},{roll}"
+                data = f"{yaw},{pitch},{roll}"
                 print(emg_float)
                 print(euler_angles)
-                # sock.sendall(data.encode("utf-8"))
-                # response = sock.recv(1024).decode("utf-8")
-                # print(response)
+                sock.sendall(data.encode("utf-8"))
+                response = sock.recv(1024).decode("utf-8")
+                print(response)
 
     except KeyboardInterrupt:
         print("Exiting loop.")
